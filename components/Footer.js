ce535c90fd25d4018189aee4ce41dffe2fd3903c
agileKat <37515947+agileKat@users.noreply.github.com>
2022-10-10 13:55:35 -0400

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,8 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        ** <img src="/underconstruction.png" alt="construction" className={styles.logo} /> **
+        ** <img src="/underconstruction.png" alt="construction" className={styles.logo} />
+           <img src="/augustus_invictus_law.jpg" alt="construction" className={styles.photo} />**
       </footer>
     </>
   )
