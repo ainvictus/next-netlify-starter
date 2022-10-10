2e101bf281b1ab5810b1ca81e37dffa94a41e268
agileKat <37515947+agileKat@users.noreply.github.com>
2022-10-10 11:36:04 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Augustus" />
         <p className="description">
           Augustus <code>pages/index.js</code>
         </p>
